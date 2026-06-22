284ceed31fe88889a0e73b0106dc52bf04789871
Akanksha <akankshak.1890@gmail.com>
2026-06-22 23:38:13 +0530

    Add files via upload

---
+++ Day-8/Ques29.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, j;
+    printf("How much rows of half pyramid:");
+    scanf("%d", &num);
+
+    for(i=1;  i<=num; i++){
+        for(j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
